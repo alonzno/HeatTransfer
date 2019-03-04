d08c3371561b7bea545bd607d9055ee3cd1a80fc
alonzno <alonzoa@cs.uoregon.edu>
2019-03-03 17:01:55 -0800

    Downgradt Version of vtk

--- ReadPLY.cxx
+++ ReadPLY.cxx
@@ -2,10 +2,145 @@
 #include <vtkPLYReader.h>
 #include <vtkSmartPointer.h>
 #include <vtkPolyDataMapper.h>
+#include <vtkOpenGLPolyDataMapper.h>
 #include <vtkActor.h>
+#include <vtkProperty.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderer.h>
 #include <vtkRenderWindowInteractor.h>
+#include <vtkPolyDataReader.h>
+
+#include <vtkFloatArray.h>
+#include <vtkPoints.h>
+#include <vtkCellArray.h>
+#include <vtkPointData.h>
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Triangle 
+{
+    public:
+        double X[3];
+        double Y[3];
+        double Z[3];
+        double fieldValue[3];
+
+        double normals[3][3];
+};
+
+std::vector<Triangle>
+GetTriangles(std::string inputFilename) {
+
+    vtkPLYReader *reader = vtkPLYReader::New();
+    reader->SetFileName ( inputFilename.c_str() );
+    cerr << "Reading" << endl;
+    reader->Update();
+    cerr << "Done reading" << endl;
+    if (reader->GetOutput()->GetNumberOfCells() == 0) {
+        cerr << "Unable to open file!!" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    vtkPolyData *pd = reader->GetOutput();
+
+    int numTris = pd->GetNumberOfCells();
+    vtkPoints *pts = pd->GetPoints();
+    vtkCellArray *cells = pd->GetPolys();
+    
+    vtkFloatArray *n = (vtkFloatArray *) pd->GetPointData()->GetNormals();
+    float *normals = n->GetPointer(0);
+    std::vector<Triangle> tris(numTris);
+    vtkIdType npts;
+    vtkIdType *ptIds;
+    int idx;
+    for (idx = 0, cells->InitTraversal(); cells->GetNextCell(npts, ptIds); idx++) {
+        if (npts != 3) {
+            cerr << "Non-triangles!! ???" << endl;
+            exit(EXIT_FAILURE);
+        }
+        double *pt = NULL;
+        pt = pts->GetPoint(ptIds[0]);
+        tris[idx].X[0] = pt[0];
+        tris[idx].Y[0] = pt[1];
+        tris[idx].Z[0] = pt[2];
+        tris[idx].normals[0][0] = normals[3*ptIds[0]+0];
+        tris[idx].normals[0][1] = normals[3*ptIds[0]+1];
+        tris[idx].normals[0][2] = normals[3*ptIds[0]+2];
+        tris[idx].fieldValue[0] = 1.0;  //TODO FIX THIS;
+        pt = pts->GetPoint(ptIds[1]);
+        tris[idx].X[1] = pt[0];
+        tris[idx].Y[1] = pt[1];
+        tris[idx].Z[1] = pt[2];
+        tris[idx].normals[1][0] = normals[3*ptIds[1]+0];
+        tris[idx].normals[1][1] = normals[3*ptIds[1]+1];
+        tris[idx].normals[1][2] = normals[3*ptIds[1]+2];
+        tris[idx].fieldValue[1] = 1.0;  //TODO FIX THIS;
+        pt = pts->GetPoint(ptIds[2]);
+        tris[idx].X[2] = pt[0];
+        tris[idx].Y[2] = pt[1];
+        tris[idx].Z[2] = pt[2];
+        tris[idx].normals[2][0] = normals[3*ptIds[2]+0];
+        tris[idx].normals[2][1] = normals[3*ptIds[2]+1];
+        tris[idx].normals[2][2] = normals[3*ptIds[2]+2];
+        tris[idx].fieldValue[2] = 1.0;  //TODO FIX THIS;
+    }
+
+    return tris;
+
+}
+
+class vtkBunnyMapper : public vtkOpenGLPolyDataMapper
+{
+    public:
+        GLuint displayList;
+        bool initialized;
+        std::vector<Triangle> tris;
+    public:
+        vtkBunnyMapper()
+        {
+            initialized = false;
+            tris = GetTriangles("./bunny/reconstruction/bun_zipper.ply");
+        }
+        
+        void RemoveVTKOpenGLStateSideEffects()
+        {
+     float Info[4] = { 0, 0, 0, 1 };
+     glLightModelfv(GL_LIGHT_MODEL_AMBIENT, Info);
+     float ambient[4] = { 1,1, 1, 1.0 };
+     glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient);
+     float diffuse[4] = { 1, 1, 1, 1.0 };
+     glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse);
+     float specular[4] = { 1, 1, 1, 1.0 };
+     glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular);
+   }
+
+
+   void SetupLight(void)
+   {
+       glEnable(GL_LIGHTING);
+       glEnable(GL_LIGHT0);
+       GLfloat diffuse0[4] = { 0.8, 0.8, 0.8, 1 };
+       GLfloat ambient0[4] = { 0.2, 0.2, 0.2, 1 };
+       GLfloat specular0[4] = { 0.0, 0.0, 0.0, 1 };
+       GLfloat pos0[4] = { 1, 2, 3, 0 };
+       glLightfv(GL_LIGHT0, GL_POSITION, pos0);
+       glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse0);
+       glLightfv(GL_LIGHT0, GL_AMBIENT, ambient0);
+       glLightfv(GL_LIGHT0, GL_SPECULAR, specular0);
+       glDisable(GL_LIGHT1);
+       glDisable(GL_LIGHT2);
+       glDisable(GL_LIGHT3);
+       glDisable(GL_LIGHT5);
+       glDisable(GL_LIGHT6);
+       glDisable(GL_LIGHT7);
+   }
+
+
+
+};
 
 int main ( int argc, char *argv[] )
 {
@@ -29,6 +164,7 @@ int main ( int argc, char *argv[] )
   vtkSmartPointer<vtkActor> actor =
     vtkSmartPointer<vtkActor>::New();
   actor->SetMapper(mapper);
+  actor->GetProperty()->SetColor(1.0, 0.0, 0.0);
 
   vtkSmartPointer<vtkRenderer> renderer =
     vtkSmartPointer<vtkRenderer>::New();
